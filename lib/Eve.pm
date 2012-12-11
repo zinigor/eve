96307c71708fcc05ccdb2aebdce62d731b31a36b
Igor Zinovyev <zinigor@gmail.com>
2012-12-11 23:09:36 +0400

    Created an initial repository with Module::Starter.

---
+++ lib/Eve.pm
@@ -0,0 +1,111 @@
+package Eve;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Eve - The great new Eve!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Eve;
+
+    my $foo = Eve->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Igor Zinovyev, C<< <zinigor at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-eve at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Eve>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Eve
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Eve>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Eve>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Eve>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Eve/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Igor Zinovyev.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Eve
