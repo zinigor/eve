234ede1982fe50d091f87d01d0e6eb45c74c30f4
Igor Zinovyev <zinigor@gmail.com>
2013-03-28 08:49:50 +0400

    Fixed the manifest and bumped the version.

--- lib/Eve.pm
+++ lib/Eve.pm
@@ -10,11 +10,11 @@ Eve - The web service creation framework written with events in mind.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
