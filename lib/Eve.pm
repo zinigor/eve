a0310efc24415e451d5987beb82f78a0127c9acc
Igor Zinovyev <zinigor@gmail.com>
2014-05-05 14:33:44 +0400

    Bumped version.

--- lib/Eve.pm
+++ lib/Eve.pm
@@ -10,11 +10,11 @@ Eve - The web service creation framework written with events in mind.
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 
 =head1 SYNOPSIS
